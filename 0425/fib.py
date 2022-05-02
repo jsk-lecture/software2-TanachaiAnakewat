cf87686053d80a059f38752a183439331978d3db
TanachaiAnakewat <thunanakewat@gmail.com>
2022-05-02 22:56:48 +0900

    0425 done

--- 0425/fib.py
+++ 0425/fib.py
@@ -1,7 +1,10 @@
 import sys
 import resource
 
+step = 0
 def fib(n):
+    global step
+    step = step+ 1
     if n == 0:
         return 0
     elif n == 1:
@@ -15,4 +18,4 @@ print("call fib({})".format(n))
 r = fib(n)
 print(" --> {}".format(r))
 
-print("memory usage = {}".format(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss))
+print("memory usage = {}, number of step = {}".format(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss, step))
--- 0425/plus.py
+++ 0425/plus.py
@@ -4,19 +4,26 @@ def dec(x):
     return x - 1
 
 def plus_a(a, b):
+    #print("a = {}, b = {} ".format(a,b))
     if a == 0:
         return b
     else:
+        print("plus({},{})=plus_a({},{})+1".format(a,b,a-1,b))
         return inc(plus_a(dec(a), b))
 
 def plus_b(a, b):
+    #print("a = {}, b = {} ".format(a,b))
     if a == 0:
         return b
     else:
+        print("plus({},{})=plus_b({},{})".format(a,b,a-1,b+1))
         return plus_b(dec(a), inc(b))
 
 
 print("call plus_a(4, 5))")
 print(plus_a(4, 5))
+print("plus_a is a iterative function")
 print("call plus_b(4, 5))")
 print(plus_b(4, 5))
+print("plus_b is a recursive function")
+
